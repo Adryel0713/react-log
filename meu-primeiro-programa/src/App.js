e01eea17583d2859304f1fa05daf0a9db6764bc9
Adryel0713 <clauber.adriel@gmail.com>
2025-07-03 23:47:51 -0300

    State Lift

--- meu-primeiro-programa/src/App.js
+++ meu-primeiro-programa/src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
-import OutraLista from './components/OutraLista';
+import { useState } from 'react';
+import SeuNome from './components/SeuNome';
+import Saudacao from './components/Saudacao';
 
 function App() {
   
-  const meusItens = ["React","Vue","Angular"];
+      const [nome,setNome] = useState()
 
   return (
     <div className="App"> 
-      <OutraLista itens={meusItens}/>
-      <OutraLista itens={[]}/>
+      <h1>State Lift</h1>
+      <SeuNome setNome={setNome}/>
+      <Saudacao nome={nome}/>
     </div>
   );
 }
